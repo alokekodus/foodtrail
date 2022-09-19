3cdbbdeac59601012a68c99cf670f4b6cc91fd8e
Alok <alok.deka@ekodus.com>
2022-09-19 14:55:17 +0530

    Manas page created

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(window).on("load", function () {
   jQuery(".loader").fadeOut(1000);
 });
 
-const swiper = new Swiper(".swiper", {
+const food_swiper = new Swiper(".featured_food_slider", {
   autoplay: {
     delay: 3000,
   },
@@ -14,7 +14,7 @@ const swiper = new Swiper(".swiper", {
     el: ".custom_pagination",
     // type: "bullets",
     type: "custom",
-    renderCustom: function (swiper, current, total) {
+    renderCustom: function (food_swiper, current, total) {
       return ("0" + current).slice(-2) + " of " + ("0" + total).slice(-2);
     },
   },
@@ -38,6 +38,35 @@ const swiper = new Swiper(".swiper", {
     1200: {
       slidesPerView: 4,
       spaceBetween: 20,
+    },
+  },
+});
+
+
+const manas_swiper = new Swiper(".manas_slider", {
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  speed: 400,
+  slidesPerView: 4,
+  spaceBetween: 0,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
     },
   },
 });
